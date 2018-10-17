ce230e144f9d6e7f58c1b7095e4ec2bd10bf8895
Shahrukh <shahrukh@webklipper.com>
2018-10-17 20:48:36 +0530

    update application class with webengage config while initializing segment

--- app/src/main/java/com/webengage/segmentintegrationexample/WebEngageSegmentIntegrationApplication.java
+++ app/src/main/java/com/webengage/segmentintegrationexample/WebEngageSegmentIntegrationApplication.java
@@ -3,6 +3,7 @@ package com.webengage.segmentintegrationexample;
 import android.app.Application;
 
 import com.segment.analytics.Analytics;
+import com.webengage.sdk.android.WebEngageConfig;
 import com.webengage.sdk.android.integrations.segment.WebEngageIntegration;
 
 /**
@@ -14,10 +15,15 @@ public class WebEngageSegmentIntegrationApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Analytics analytics = new Analytics.Builder(this, "UQNe5yhVnTtyVi6RGRFAlvG9WFEVagA6")
+        WebEngageConfig webEngageConfig = new WebEngageConfig.Builder()
+                .setDebugMode(true)
+                .setPushLargeIcon(R.mipmap.ic_launcher)
+                .setPushSmallIcon(R.mipmap.ic_launcher_round)
+                .build();
+        Analytics analytics = new Analytics.Builder(this, "YOUR_WRITE_KEY")
                 .trackApplicationLifecycleEvents() // Enable this to record certain application events automatically!
                 .logLevel(Analytics.LogLevel.VERBOSE)
-                .use(WebEngageIntegration.FACTORY)
+                .use(WebEngageIntegration.FACTORY.withWebEngageConfig(webEngageConfig))
                 .build();
         Analytics.setSingletonInstance(analytics);
     }
